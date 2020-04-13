b25cf1f42f38be36741e0bc5e10822590f535831
Deepak N <deepaknagesh05@gmail.com>
2020-04-13 13:13:15 +0530

    Deploy gh-pages

---
+++ out/_next/static/M28uit9FJ85IeC0C9ukVX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/2":[a],"/index":[a]}}("static\u002Fchunks\u002F9e2b914a03acf03c202ea1f9a0c0442de582feb6.152c8d9cd1182b086cde.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
