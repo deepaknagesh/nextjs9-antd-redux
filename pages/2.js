6297d28dd38e8968a393e5e596e96a9be4efb856
Deepak N <deepaknagesh05@gmail.com>
2020-04-17 14:20:04 +0530

    Code refactoring.

--- pages/2.js
+++ pages/2.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { loadData, startClock, tickClock } from '../redux/tickers/actions'
-import Page from '../components/page'
+import Page2 from '../components/page'
+import Page from "../hocs/defaultPage";
 
 class Two extends React.Component {
     static async getInitialProps(props) {
@@ -21,8 +22,10 @@ class Two extends React.Component {
     }
 
     render() {
-        return <Page title="Index Page" linkTo="/other" NavigateTo="Other Page" />
+        return <Page2 title="Index Page" linkTo="/" NavigateTo="Other Page" />
     }
 }
+
+// export default connect()(Page(Two))
 
 export default connect()(Two)
